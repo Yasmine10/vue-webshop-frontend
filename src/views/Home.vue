30b8f249fa5f35d3fd8ac5859228c35caa40c72c
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-22 02:27:20 +0200

    added images to the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="home"></div>
+  <div class="home padding-page">
+    <div class="image-container">
+      <div class="home__header container">
+        <h1>Welkom!</h1>
+        <img class="home-image" src="../assets/images/home-banner2.png" alt="dogs in a row" />
+      </div>
+    </div>
+    <div class="container">
+      <h3>Home page in opbouw</h3>
+      <div class="promo">
+        <div>
+          <img src="../assets/images/home-catfood.jpg" alt="catfood" />
+        </div>
+        <div>
+          <img src="../assets/images/home-catfood2.jpg" alt="catfood" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +26,47 @@ export default {
   components: {},
   created() {
     this.$store.dispatch("isCheckoutSuccess", false);
-  }
+  },
 };
 </script>
+
+<style scoped lang="scss">
+.image-container {
+  background-color: hsl(207, 56%, 89%);
+  text-align: center;
+  margin-bottom: 3rem;
+
+  h1 {
+    font-family: var(--ff-accent);
+    font-size: 42pt;
+    color: var(--clr-primary-independence);
+    padding-top: 2rem;
+  }
+}
+
+.home {
+  &__header {
+  
+  }
+  
+  h3 {
+    text-align: center;
+    padding-bottom: 1em;
+  }
+}
+
+.home-image {
+  max-width: 60%;
+  margin: 0 auto;
+}
+
+.promo {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+}
+</style>
